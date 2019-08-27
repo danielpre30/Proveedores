2e9c886d9f0469c80a9b9299c0d341ae68ac658b
izuluagag1998 <izuluagag1998@gmail.com>
2019-08-27 00:01:15 -0500

    Integracion formulario pago prueba

---
+++ src/pages/PaymentPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/LandingPage.css';
+import PaymentSection from '../sections/PaymentSection';
+
+function PaymentPage(){
+   
+    return(
+        <>
+            <PaymentSection/>
+        </>
+        
+    );
+    
+}
+
+export default PaymentPage;
