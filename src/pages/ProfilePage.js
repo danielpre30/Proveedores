45f2a78b2b2af4368b330f06643f4b5fb8046705
izuluagag1998 <izuluagag1998@gmail.com>
2019-10-14 16:15:11 -0500

    Cambio posicionamiento elementos  perfil

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,32 +1,28 @@
 import React from "react";
 import "../styles/LandingPage.css";
 import "../styles/HomePage.css";
-
 import LandingSection from "../sections/LandingSection";
-import ProvidersSection from "../sections/ProvidersSection";
-import PaymentSection from "../sections/PaymentSection";
 import ProfileSection from "../sections/ProfileSection";
 import Scroll from "../resources/scroll.gif";
 import { useAuth0 } from "../Auth/react-auth0-wrapper";
 
 function ProfilePage() {
-    const { isAuthenticated } = useAuth0();
-    return (
-        <>
-            {isAuthenticated ? (
-                <>
-                    <LandingSection />
-                    <img className="scrollImage" src={Scroll} alt="Scroll Gif" />
-                    <ProvidersSection />
-                </>
-            ) : (
-                    <>
+  const { isAuthenticated } = useAuth0();
 
-                        <ProfileSection />
-                    </>
-                )}
+  return (
+    <>
+      {isAuthenticated ? (
+        <>
+          <LandingSection />
+          <img className="scrollImage" src={Scroll} alt="Scroll Gif" />
         </>
-    );
+      ) : (
+        <>
+          <ProfileSection />
+        </>
+      )}
+    </>
+  );
 }
 
 export default ProfilePage;
--- src/sections/ProfileSection.js
+++ src/sections/ProfileSection.js
@@ -1,146 +1,142 @@
 import React, { Component } from "react";
-import "../styles/ProvidersSection.css";
+
 import "../styles/ProfileSection.css";
 import star from "../resources/star.png";
 
 class ProfileSection extends Component {
-    render() {
-        return (
+  render() {
+    return (
+      <div className="ProfileCanvas">
+        <div className="TitleText">
+          <h1>Nombre empresa</h1>
+        </div>
 
-            <div className="ProfileCanvas">
+        <div className="SubtitleText">
+          <h1>Tipo de servicio</h1>
+        </div>
 
-                <div className="TitleText">
-                    <h1 >Nombre empresa</h1>
-                </div>
-                <div className="SubtitleText">
-                    <h1 >Tipo de servicio</h1>
-                </div>
+        <div className="ProfilePic"></div>
 
-                <div className="ProfilePic">
+        <div className="RateBox">
+          <h1 className="RateText">4,9</h1>
+          <img className="Ratestar" src={star} alt="RateStar" />
+        </div>
 
-                </div>
+        <div className="PuntualityStar">
+          <ul>
+            <h3>Puntualidad</h3>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+          </ul>
+        </div>
 
-                <div className="PuntualityStar">
-                    <ul >
-                        <h3>Puntualidad</h3>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
+        <div className="ComunicationStar">
+          <ul>
+            <h3>Comunicación</h3>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+          </ul>
+        </div>
 
-                    </ul>
-                </div>
+        <div className="ServiceStar">
+          <ul>
+            <h3>Servicio Posventa</h3>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+          </ul>
+        </div>
 
-                <div className="ComunicationStar">
-                    <ul >
-                        <h3>Comunicación</h3>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
+        <div className="QualityStar">
+          <ul>
+            <h3>Calidad/Precio</h3>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+            <li>
+              <img className="star" src={star} alt="starPuntuality" />
+            </li>
+          </ul>
+        </div>
 
-                    </ul>
-                </div>
+        <div className="OpinionsChart">
+          <div className="NavBarOptions">
+            <ul>
+              <li>
+                <input
+                  className="button-GoOpinions"
+                  name="Submit"
+                  type="submit"
+                  value="opiniones"
+                />
+              </li>
+              <li>
+                <input
+                  className="button-GoOpinions"
+                  name="Submit"
+                  type="submit"
+                  value="Proveedores"
+                />
+              </li>
+            </ul>
+          </div>
 
-                <div className="ServiceStar">
-                    <ul >
-                        <h3>Servicio Posventa</h3>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className="QualityStar">
-                    <ul>
-                        <h3 >Calidad/Precio</h3>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-                        <li>
-                            <img className="star" src={star} alt="starPuntuality" />
-                        </li>
-
-                    </ul>
-                </div>
-
-                <div className="OpinionsChart">
-                    <div className="NavBarOptions">
-                        <ul>
-                            <li>
-                                <input
-                                    className="button-GoOpinions"
-                                    name="Submit"
-                                    type="submit"
-                                    value="opiniones"
-                                />
-                            </li>
-                            <li>
-                                <input
-                                    className="button-GoOpinions"
-                                    name="Submit"
-                                    type="submit"
-                                    value="Proveedores"
-                                />
-
-                            </li>
-
-                        </ul>
-                    </div>
-
-                    <p>   Mi lista de proveedores, proveedor actual, proveedor contrato pasado, reporte de errores</p>
-
-                </div>
-
-            </div>
-
-
-        );
-    }
+          <p>
+            {" "}
+            Mi lista de proveedores, proveedor actual, proveedor contrato
+            pasado, reporte de errores
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ProfileSection;
